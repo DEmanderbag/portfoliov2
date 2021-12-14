7441cfd30a5d0a94a03469f57fc6182ce94c577b
DEmanderbag <dusan-milosevic@outlook.com>
2021-12-14 22:48:40 +0100

    Add review card component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
 import Head from "next/head";
 import Image from "next/image";
+import ImageGrid from "../components/ImageGrid";
+import ReviewCard from "../components/ReviewCard";
 import styles from "../styles/Home.module.scss";
 
+const data1 = [
+  {
+    image: "/copyright-paulstamatiou_com-DSC6240-1000.webp",
+    className: "m-span-6 full",
+  },
+  {
+    image: "/copyright-paulstamatiou_com-DSC6240-1000.webp",
+    className: "m-span-3 span-6",
+  },
+  {
+    image: "/copyright-paulstamatiou_com-DSC6240-1000.webp",
+    className: "m-span-3 span-6",
+  },
+  {
+    image: "/copyright-paulstamatiou_com-DSC6240-1000.webp",
+    className: "m-span-3 full",
+  },
+];
+
+const data2 = [
+  {
+    image: "/copyright-paulstamatiou_com-DSC6275-2500.webp",
+    className: "m-span-4 span-4",
+  },
+  {
+    image: "/copyright-paulstamatiou_com-DSC6275-2500.webp",
+    className: "m-span-2 span-4",
+  },
+  {
+    image: "/copyright-paulstamatiou_com-DSC6275-2500.webp",
+    className: "m-span-6 span-4",
+  },
+];
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,58 +47,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1>Hello world</h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {/* <ImageGrid data={data1} /> */}
+        <p>Just some random text section</p>
+        <ReviewCard />
+        {/* <ImageGrid data={data2} /> */}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
