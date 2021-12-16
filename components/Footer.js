16d28681f421d2aed4dc34bd7ff8bc5fcc063229
DEmanderbag <dusan-milosevic@outlook.com>
2021-12-16 21:54:23 +0100

    Add first version of footer styles

---
+++ components/Footer.js
@@ -0,0 +1,80 @@
+import styles from "../styles/Footer.module.scss";
+
+const footerData = [
+  {
+    question: "Ready to make something kickass?",
+    action: "Let's get on a call.",
+    name: "Portolfio Creator",
+    email: "dusan-milosevic@outlook.com",
+    links: [
+      {
+        name: "About",
+        path: "/about",
+      },
+      {
+        name: "Links",
+        path: "/links",
+      },
+      {
+        name: "Blog",
+        path: "/blog",
+      },
+      {
+        name: "Projects",
+        path: "/projects",
+      },
+      {
+        name: "Contact",
+        path: "/contact",
+      },
+      {
+        name: "Hardware I use",
+        path: "/links",
+      },
+      {
+        name: "Software I use",
+        path: "/about",
+      },
+      {
+        name: "Bookmarks",
+        path: "/links",
+      },
+      {
+        name: "Goals 2022",
+        path: "/links",
+      },
+    ],
+  },
+];
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      {footerData.map((value) => (
+        <div className={styles.wrapper} key={value}>
+          <div className={styles.message}>
+            <h2>{value.question}</h2>
+            <h3 className="test">{value.action}</h3>
+          </div>
+          <div className={styles.group}>
+            <div className={styles.contact}>
+              <p>
+                {value.name}
+                <span className="test">.</span>
+              </p>
+              <a href={`mailto:${value.email}`}>{value.email}</a>
+            </div>
+            <ul className={styles.links}>
+              {value.links.map((link) => (
+                <li key={link.name}>
+                  <a href={link.path}>{link.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
+    </footer>
+  );
+};
+
+export default Footer;
