a4e7fb39d05dec17b14e1c9c9ba8b5eccc192a6e
DEmanderbag <dusan-milosevic@outlook.com>
2022-01-16 13:12:53 +0100

    Update poster UI

--- components/Poster.js
+++ components/Poster.js
@@ -4,21 +4,27 @@ import Link from "next/link";
 const posterData = [
   {
     path: "blog",
-    title: "User interface design",
+    linkText: "Experience",
+    title: "Things to do on your trip",
     info: "Belajar Ul Design dari dasar",
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1511135570219-bbad9a02f103?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
   },
   {
     path: "blog",
-    title: `Book marks`,
+    linkText: "Find more",
+    title: `Bookmarks`,
     info: "Belajar Ul Design dari dasar",
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
   },
   {
     path: "blog",
+    linkText: "Travel Experiences",
     title: "Travel blog",
     info: "Belajar Ul Design dari dasar",
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1523906921802-b5d2d899e93b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=953&q=80",
   },
 ];
 
@@ -26,13 +32,20 @@ const Poster = () => {
   return (
     <section className={styles.poster}>
       {posterData.map((poster) => (
-        <article key={poster.path} className={styles.posterCard}>
-          <Link href={poster.path}>
-            <a>
-              <h1 className={styles.title}>{poster.title}</h1>
-              <p className={styles.info}>{poster.info}</p>
-            </a>
-          </Link>
+        <article
+          key={poster.path}
+          className={styles.posterCard}
+          style={{ backgroundImage: `url(${poster.image})` }}
+        >
+          <div className={styles.wrapper}>
+            <h2 className={styles.title}>{poster.title}</h2>
+            <p className={styles.info}>{poster.info}</p>
+            <Link href={poster.path}>
+              <a>
+                <div className={styles.link}>{poster.linkText}</div>
+              </a>
+            </Link>
+          </div>
         </article>
       ))}
     </section>
