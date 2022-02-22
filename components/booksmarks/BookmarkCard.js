bc266879cb4da818fe008dd5b2abcf12983f56f7
DEmanderbag <dusan-milosevic@outlook.com>
2022-02-22 23:36:01 +0100

    Add intersection observer / animations / hover state, Update colors Remove code that was not used

--- components/booksmarks/BookmarkCard.js
+++ components/booksmarks/BookmarkCard.js
@@ -1,30 +1,48 @@
 import Image from "next/image";
 import styles from "../../styles/bookmarks/BookmarkCard.module.scss";
+import { useState, useEffect, useRef, createRef } from "react";
+
+function useOnScreen(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(`${styles.animated}`);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { rootMargin: "0px 0px -100px 0px" }
+    );
+
+    ref.forEach((element) => {
+      observer.observe(element.current);
+    });
+  }, [ref]);
+  return isIntersecting;
+}
 
 const BookmarkCard = ({ devResources }) => {
+  const refs = useRef([]);
+  const refsArray = (refs.current = devResources.map(
+    (element, i) => refs.current[i] ?? createRef()
+  ));
+  const onScreen = useOnScreen(refsArray);
+
   return (
     <>
-      {devResources.map((resource) => (
+      {devResources.map((resource, i) => (
         <a
-          key={resource.title}
+          key={i}
           href={resource.website}
-          className={styles.cardBasic}
+          ref={refs.current[i]}
+          className={`${styles.cardBasic} ${onScreen ? styles.animated : ""}`}
           target="_blank"
           rel="noreferrer"
         >
           <article>
-            {/* <div className={styles.favicon}>
-            <div className={styles.faviconMask}>
-              <Image
-                src={`${resource.logo}`}
-                alt="test image"
-                layout="responsive"
-                width={40}
-                height={40}
-                blurDataURL={`${resource.logo}`}
-              />
-            </div>
-          </div> */}
             <div className={styles.cardContent}>
               <p
                 style={{ backgroundColor: `${resource.tagColor}` }}
--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -85,7 +85,7 @@ const devResources = [
     website: "https://developer.apple.com/videos/play/wwdc2018/803/",
     websiteShort: "developer.apple.com",
     tag: "Video",
-    tagColor: "#f2e6e6",
+    tagColor: "#EC5555",
   },
   {
     title: "Design for spatial interaction",
@@ -94,180 +94,7 @@ const devResources = [
     website: "https://developer.apple.com/videos/play/wwdc2021/10245/",
     websiteShort: "developer.apple.com",
     tag: "Video",
-    tagColor: "#f2e6e6",
-  },
-  {
-    title: "Common webpage design mistakes",
-    subtitle:
-      "Simple layout and design tips to help you create a stunning webpage",
-    website: "https://uxplanet.org/common-webpage-design-mistakes-59eed9831bd7",
-    websiteShort: "uxplanet.org",
-    tag: "Article",
-    tagColor: "#e6f2ee",
-  },
-  {
-    title: "Brian Lovin",
-    subtitle: "Brian is a designer, podcaster, writer and software tinkerer",
-    website: "https://brianlovin.com/",
-    websiteShort: "brianlovin.com",
-    tag: "Portfolio",
-    tagColor: "#f2eee6",
-  },
-  {
-    title: "Rauno Freiberg",
-    subtitle: "Rauno is a design engineer in love with emotional interfaces.",
-    website: "https://rauno.me/",
-    websiteShort: "rauno.me",
-    tag: "Portfolio",
-    tagColor: "#f2eee6",
-  },
-  {
-    title: "racOS",
-    subtitle: "A web3 enabled portal for all $RAC holders.",
-    website: "https://rac.fm/",
-    websiteShort: "rac.fm",
-    tag: "Portal",
-    tagColor: "#f2eee6",
-  },
-  {
-    title: "Open Doodles",
-    subtitle: "A Free Set of Open-Source Illustrations!",
-    website: "https://www.opendoodles.com/",
-    websiteShort: "opendoodles.com",
-    tag: "Tools",
-    tagColor: "#e8e6f2",
-  },
-  {
-    title: "Ouch",
-    subtitle: "Free vector illustrations to enhance your project",
-    website: "https://icons8.com/illustrations",
-    websiteShort: "icons8.com",
-    tag: "Tools",
-    tagColor: "#e8e6f2",
-  },
-  {
-    title: "Unsplash",
-    subtitle:
-      "The internet’s source of freely-usable images. Powered by creators everywhere.",
-    website: "https://unsplash.com/",
-    websiteShort: "unsplash.com",
-    tag: "Tools",
-    tagColor: "#e8e6f2",
-  },
-  {
-    title: "Coverr",
-    subtitle: "Beautiful Free Stock Video Footage",
-    website: "https://coverr.co/",
-    websiteShort: "coverr.co",
-    tag: "Tools",
-    tagColor: "#f2eee6",
-  },
-  {
-    title: "Carbon",
-    subtitle:
-      "Create and share beautiful images of your source code. Start typing or drop a file into the text area to get started.",
-    website: "https://carbon.now.sh/",
-    websiteShort: "carbon.now.sh",
-    tag: "Tools",
-    tagColor: "#e8e6f2",
-  },
-  {
-    title: "CSS clip-path maker",
-    subtitle:
-      "The clip-path property allows you to make complex shapes in CSS by clipping an element to a basic shape (circle, ellipse, polygon, or inset), or to an SVG source.",
-    website: "https://bennettfeely.com/clippy/",
-    websiteShort: "bennettfeely.com/clippy",
-    tag: "Tools",
-    tagColor: "#e8e6f2",
-  },
-  {
-    title: "WebGradients",
-    subtitle:
-      "WebGradients is a free collection of 180 linear gradients that you can use as content backdrops in any part of your website. Easy copy CSS3 crossbrowser code and use it in a moment!",
-    website: "https://webgradients.com/",
-    websiteShort: "webgradients.com",
-    tag: "Tools",
-    tagColor: "#e8e6f2",
-  },
-  {
-    title: "🔥 Design Tips",
-    subtitle:
-      "A collection of design tips created by Steve Schoger and Adam Wathan",
-    website: "https://twitter.com/i/events/994601867987619840",
-    websiteShort: "",
-    tag: "Tools",
-    tagColor: "#e8e6f2",
-  },
-  {
-    title: "Shortcuts.design",
-    subtitle: "Every shortcut for designers, centralized and searchable.",
-    website: "https://shortcuts.design/index.html",
-    websiteShort: "shortcuts.design",
-    tag: "Tools",
-    tagColor: "#e8e6f2",
-  },
-  {
-    title: "99% Invisible",
-    subtitle:
-      "Design is everywhere in our lives, perhaps most importantly in the places where we've just stopped noticing. 99% Invisible is a weekly exploration of the process and power of design and architecture. This podcast is hosted by Roman mars",
-    website: "https://99percentinvisible.org/",
-    websiteShort: "99percentinvisible.org",
-    tag: "Podcast",
-    tagColor: "#ffffa7",
-  },
-  {
-    title: "Command_Line Heroes",
-    subtitle:
-      "Hear the epic true tales of how developers, programmers, hackers, geeks, and open source rebels are revolutionizing the technology landscape. An original podcast from Red Hat",
-    website: "https://www.redhat.com/en/command-line-heroes",
-    websiteShort: "redhat.com/en/command-line-heroes",
-    tag: "Podcast",
-    tagColor: "#ffffa7",
-  },
-  {
-    title: "Cortex",
-    subtitle:
-      "CGP Grey and Myke Hurley are both independent content creators. Each episode, they get together to discuss their working lives.",
-    website: "https://www.relay.fm/cortex",
-    websiteShort: "relay.fm/cortex",
-    tag: "Podcast",
-    tagColor: "#ffffa7",
-  },
-  {
-    title: "3%",
-    subtitle:
-      "In order to make something great you only need to change the existing layer by 3%. Article written by Mariano Avila a who is a product designed",
-    website: "https://mar.svbtle.com/3",
-    websiteShort: "mar.svbtle.com/3",
-    tag: "Article",
-    tagColor: "#e6f2ee",
-  },
-  {
-    title: "Kevin Powell",
-    subtitle:
-      "Helping you learn how to make the web, and make it look good while you're at it. With videos every Tuesday and Thursday, I'll be bringing you How Tos and Tutorials, as well as simple tips and tricks, with a big focus on helping people see how wonderful CSS is!",
-    website: "https://www.youtube.com/kepowob",
-    websiteShort: "youtube.com/kepowob",
-    tag: "YouTube",
-    tagColor: "#f2e6e6",
-  },
-  {
-    title: "LEMMiNO",
-    subtitle:
-      "Documentaries and list videos narrated by a 20-something Swedish guy.",
-    website: "https://www.youtube.com/c/LEMMiNO/",
-    websiteShort: "youtube.com/c/LEMMiNO",
-    tag: "YouTube",
-    tagColor: "#f2e6e6",
-  },
-  {
-    title: "Colt Steele",
-    subtitle:
-      "Colt Steele is a Developer and Bootcamp Instructor, who spent years figuring out the 'formula' to teaching technical skills in a classroom environment, I can confidently say that his online courses are without a doubt the most comprehensive ones on the market.",
-    website: "https://www.youtube.com/c/ColtSteeleCode",
-    websiteShort: "youtube.com/c/ColtSteeleCode",
-    tag: "YouTube",
-    tagColor: "#f2e6e6",
+    tagColor: "#EC5555",
   },
   {
     title: "Introduction to Git",
@@ -277,7 +104,7 @@ const devResources = [
       "https://www.notion.so/zarkom/Introduction-to-Git-ac396a0697704709a12b6a0e545db049",
     websiteShort: "",
     tag: "Article",
-    tagColor: "#e6f2ee",
+    tagColor: "#22B07D",
   },
   {
     title: "Introduction to GitHub",
@@ -287,7 +114,7 @@ const devResources = [
       "https://www.notion.so/zarkom/Introduction-to-GitHub-202af6f64bbd4299b15f238dcd09d2a7",
     websiteShort: "",
     tag: "Article",
-    tagColor: "#e6f2ee",
+    tagColor: "#22B07D",
   },
   {
     title: "How to Write a Git Commit Message",
@@ -296,25 +123,59 @@ const devResources = [
     website: "https://cbea.ms/git-commit/",
     websiteShort: "cbea.ms/git-commit",
     tag: "Article",
-    tagColor: "#e6f2ee",
+    tagColor: "#22B07D",
   },
   {
-    title: "Skillthrive",
-    subtitle:
-      "Learn how to use tools like Sketch, Adobe XD, Figma, HTML, CSS, Javascript, React, Gatsby and more to build real design and web development projects.",
-    website: "https://www.youtube.com/c/Skillthrive/",
-    websiteShort: "youtube.com/c/Skillthrive",
-    tag: "YouTube",
-    tagColor: "#f2e6e6",
+    title: "racOS",
+    subtitle: "A web3 enabled portal for all $RAC holders.",
+    website: "https://rac.fm/",
+    websiteShort: "rac.fm",
+    tag: "Portoflio",
+    tagColor: "#B841F0",
   },
   {
-    title: "Jake Wright",
+    title: "Open Doodles",
+    subtitle: "A Free Set of Open-Source Illustrations!",
+    website: "https://www.opendoodles.com/",
+    websiteShort: "opendoodles.com",
+    tag: "Tools",
+    tagColor: "#5956FF",
+  },
+  {
+    title: "🔥 Design Tips",
     subtitle:
-      "Jake Wright is a Cambridge Computer Science graduate, software developer, and filmmaker. He makes videos about tech, coding, cooking and travelling.",
-    website: "https://www.youtube.com/c/JakewrightNet/",
-    websiteShort: "youtube.com/c/JakewrightNet",
+      "A collection of design tips created by Steve Schoger and Adam Wathan",
+    website: "https://twitter.com/i/events/994601867987619840",
+    websiteShort: "",
+    tag: "Article",
+    tagColor: "#22B07D",
+  },
+  {
+    title: "Common webpage design mistakes",
+    subtitle:
+      "Simple layout and design tips to help you create a stunning webpage",
+    website: "https://uxplanet.org/common-webpage-design-mistakes-59eed9831bd7",
+    websiteShort: "uxplanet.org",
+    tag: "Article",
+    tagColor: "#22B07D",
+  },
+  {
+    title: "99% Invisible",
+    subtitle:
+      "Design is everywhere in our lives, perhaps most importantly in the places where we've just stopped noticing. 99% Invisible is a weekly exploration of the process and power of design and architecture. This podcast is hosted by Roman mars",
+    website: "https://99percentinvisible.org/",
+    websiteShort: "99percentinvisible.org",
+    tag: "Podcast",
+    tagColor: "#FF9B25",
+  },
+  {
+    title: "Colt Steele",
+    subtitle:
+      "Colt Steele is a Developer and Bootcamp Instructor, who spent years figuring out the 'formula' to teaching technical skills in a classroom environment, I can confidently say that his online courses are without a doubt the most comprehensive ones on the market.",
+    website: "https://www.youtube.com/c/ColtSteeleCode",
+    websiteShort: "youtube.com/c/ColtSteeleCode",
     tag: "YouTube",
-    tagColor: "#f2e6e6",
+    tagColor: "#EC5555",
   },
   {
     title: "Traversy Media",
@@ -323,17 +184,157 @@ const devResources = [
     website: "https://www.youtube.com/c/TraversyMedia/",
     websiteShort: "youtube.com/c/TraversyMedia",
     tag: "YouTube",
-    tagColor: "#f2e6e6",
+    tagColor: "#EC5555",
+  },
+  {
+    title: "3%",
+    subtitle:
+      "In order to make something great you only need to change the existing layer by 3%. Article written by Mariano Avila a who is a product designed",
+    website: "https://mar.svbtle.com/3",
+    websiteShort: "mar.svbtle.com/3",
+    tag: "Article",
+    tagColor: "#22B07D",
+  },
+  {
+    title: "Brian Lovin",
+    subtitle: "Brian is a designer, podcaster, writer and software tinkerer",
+    website: "https://brianlovin.com/",
+    websiteShort: "brianlovin.com",
+    tag: "Portfolio",
+    tagColor: "#B841F0",
+  },
+  {
+    title: "Rauno Freiberg",
+    subtitle: "Rauno is a design engineer in love with emotional interfaces.",
+    website: "https://rauno.me/",
+    websiteShort: "rauno.me",
+    tag: "Portfolio",
+    tagColor: "#B841F0",
+  },
+  {
+    title: "Ouch",
+    subtitle: "Free vector illustrations to enhance your project",
+    website: "https://icons8.com/illustrations",
+    websiteShort: "icons8.com",
+    tag: "Tools",
+    tagColor: "#5956FF",
+  },
+  {
+    title: "Unsplash",
+    subtitle:
+      "The internet’s source of freely-usable images. Powered by creators everywhere.",
+    website: "https://unsplash.com/",
+    websiteShort: "unsplash.com",
+    tag: "Tools",
+    tagColor: "#5956FF",
+  },
+  {
+    title: "Coverr",
+    subtitle: "Beautiful Free Stock Video Footage",
+    website: "https://coverr.co/",
+    websiteShort: "coverr.co",
+    tag: "Tools",
+    tagColor: "#5956FF",
+  },
+  {
+    title: "Carbon",
+    subtitle:
+      "Create and share beautiful images of your source code. Start typing or drop a file into the text area to get started.",
+    website: "https://carbon.now.sh/",
+    websiteShort: "carbon.now.sh",
+    tag: "Tools",
+    tagColor: "#5956FF",
+  },
+  {
+    title: "CSS clip-path maker",
+    subtitle:
+      "The clip-path property allows you to make complex shapes in CSS by clipping an element to a basic shape (circle, ellipse, polygon, or inset), or to an SVG source.",
+    website: "https://bennettfeely.com/clippy/",
+    websiteShort: "bennettfeely.com/clippy",
+    tag: "Tools",
+    tagColor: "#5956FF",
+  },
+  {
+    title: "WebGradients",
+    subtitle:
+      "WebGradients is a free collection of 180 linear gradients that you can use as content backdrops in any part of your website. Easy copy CSS3 crossbrowser code and use it in a moment!",
+    website: "https://webgradients.com/",
+    websiteShort: "webgradients.com",
+    tag: "Tools",
+    tagColor: "#5956FF",
+  },
+  {
+    title: "Shortcuts.design",
+    subtitle: "Every shortcut for designers, centralized and searchable.",
+    website: "https://shortcuts.design/index.html",
+    websiteShort: "shortcuts.design",
+    tag: "Tools",
+    tagColor: "#5956FF",
+  },
+  {
+    title: "Command_Line Heroes",
+    subtitle:
+      "Hear the epic true tales of how developers, programmers, hackers, geeks, and open source rebels are revolutionizing the technology landscape. An original podcast from Red Hat",
+    website: "https://www.redhat.com/en/command-line-heroes",
+    websiteShort: "redhat.com/en/command-line-heroes",
+    tag: "Podcast",
+    tagColor: "#FF9B25",
+  },
+  {
+    title: "Cortex",
+    subtitle:
+      "CGP Grey and Myke Hurley are both independent content creators. Each episode, they get together to discuss their working lives.",
+    website: "https://www.relay.fm/cortex",
+    websiteShort: "relay.fm/cortex",
+    tag: "Podcast",
+    tagColor: "#FF9B25",
+  },
+  {
+    title: "Kevin Powell",
+    subtitle:
+      "Helping you learn how to make the web, and make it look good while you're at it. With videos every Tuesday and Thursday, I'll be bringing you How Tos and Tutorials, as well as simple tips and tricks, with a big focus on helping people see how wonderful CSS is!",
+    website: "https://www.youtube.com/kepowob",
+    websiteShort: "youtube.com/kepowob",
+    tag: "YouTube",
+    tagColor: "#EC5555",
+  },
+  {
+    title: "Jake Wright",
+    subtitle:
+      "Jake Wright is a Cambridge Computer Science graduate, software developer, and filmmaker. He makes videos about tech, coding, cooking and travelling.",
+    website: "https://www.youtube.com/c/JakewrightNet/",
+    websiteShort: "youtube.com/c/JakewrightNet",
+    tag: "YouTube",
+    tagColor: "#EC5555",
+  },
+  {
+    title: "Skillthrive",
+    subtitle:
+      "Learn how to use tools like Sketch, Adobe XD, Figma, HTML, CSS, Javascript, React, Gatsby and more to build real design and web development projects.",
+    website: "https://www.youtube.com/c/Skillthrive/",
+    websiteShort: "youtube.com/c/Skillthrive",
+    tag: "YouTube",
+    tagColor: "#EC5555",
+  },
+  {
+    title: "LEMMiNO",
+    subtitle:
+      "Documentaries and list videos narrated by a 20-something Swedish guy.",
+    website: "https://www.youtube.com/c/LEMMiNO/",
+    websiteShort: "youtube.com/c/LEMMiNO",
+    tag: "YouTube",
+    tagColor: "#EC5555",
   },
 ];
+
 export default function Bookmarks() {
   return (
     <section className={styles.wrapper}>
       <div className={styles.intro}>
         <h1>Bookmarks</h1>
         <p>
-          Find the list of useful websites, great blogs, interesting ideas and
-          inspirative quotes
+          Find a list of useful websites, great blogs, interesting ideas, and
+          podcasts.
         </p>
       </div>
 
