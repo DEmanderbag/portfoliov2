82531f769058f5602055ff2d052532b17cec9555
DEmanderbag <dusan-milosevic@outlook.com>
2022-02-26 13:52:34 +0100

    Filter elements by ID, clean up code

--- pages/blog.js
+++ pages/blog.js
@@ -1,44 +1,38 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/blog/BlogCard.module.scss";
 
 const blogData = [
   {
     title: "Exploring Croatia",
-    date: "11.11.2011",
     subtitle:
       "Story how I've travel and visited four different cities in Croatia",
-    coverImage:
-      "https://images.unsplash.com/photo-1610106109549-5813ab87ae88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80",
+    coverImage: "/photo.jpeg",
+    blogLink: "/blog/exploring-croatia",
     class: "cr2",
+    id: "1",
   },
   {
     title: "Ski in Kopaonik",
-    date: "11.11.2011",
     subtitle:
       "Story how I've travel and visited four different cities in Croatia",
-    coverImage:
-      "https://images.unsplash.com/photo-1614619632105-b571e9f0b3d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    coverImage: "/winter.jpeg",
     class: "c2",
+    blogLink: "/blog/winter-at-kopaonik",
+    id: "2",
   },
   {
-    title: "Winter Holliday",
-    date: "11.11.2011",
+    title: "Exploring Budapest",
     subtitle:
       "Story how I've travel and visited four different cities in Croatia",
-    coverImage:
-      "https://images.unsplash.com/photo-1565426873118-a17ed65d74b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2376&q=80",
-    class: "",
-  },
-  {
-    title: "Time in Belgium",
-    date: "11.11.2011",
-    subtitle:
-      "Story how I've travel and visited four different cities in Croatia",
-    coverImage:
-      "https://images.unsplash.com/photo-1580712327072-d525a6a5f840?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2347&q=80",
-    class: "",
+    coverImage: "/wando.jpg",
+    blogLink: "/blog/exploring-croatia",
+    class: "cr2",
+    id: "3",
   },
 ];
+blogData.sort((a, b) => b.id - a.id);
+
 export default function Blog() {
   return (
     <section className={`globalWrapper ${styles.wrapper}`}>
@@ -49,17 +43,26 @@ export default function Blog() {
       <div className={styles.blogGrid}>
         {blogData.map((blog) => (
           <div key={blog.title} className={`${styles.article} ${blog.class}`}>
-            <article
-              className={styles.card}
-              style={{ backgroundImage: `url(${blog.coverImage})` }}
-            >
-              <Link href="#">
-                <a>
-                  <h2 className="h3">{blog.title}</h2>
-                  <p>{blog.subtitle}</p>
-                </a>
-              </Link>
-            </article>
+            <Link href={blog.blogLink}>
+              <a>
+                <article className={styles.card}>
+                  <div className={styles.cardAbout}>
+                    <h2 className="h3">{blog.title}</h2>
+                    <p>{blog.subtitle}</p>
+                  </div>
+                  <div className={styles.blogImage}>
+                    <Image
+                      src={`${blog.coverImage}`}
+                      alt=""
+                      layout="fill"
+                      placeholder="blur"
+                      blurDataURL={`${blog.coverImage}`}
+                      objectFit="cover"
+                    />
+                  </div>
+                </article>
+              </a>
+            </Link>
           </div>
         ))}
       </div>
