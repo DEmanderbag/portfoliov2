e485ad1c28a36917de11d44240829eefd723201e
DEmanderbag <dusan-milosevic@outlook.com>
2022-02-26 14:30:10 +0100

    Rename some elements for the card

--- components/blog/ReviewCard.js
+++ components/blog/ReviewCard.js
@@ -1,52 +1,59 @@
 import Image from "next/image";
 import styles from "../../styles/blog/ReviewCard.module.scss";
 
-const ReviewCard = ({ apartment }) => {
+const ReviewCard = ({ reviewCardData }) => {
   return (
     <article>
       <div className={styles.reviewCard}>
         <div className={styles.content}>
           <div className={styles.about}>
-            <h4 className={styles.title}>{apartment.title}</h4>
+            <h4 className={styles.title}>{reviewCardData.title}</h4>
             <span className={styles.description}>
-              {apartment.propertyAddress}
+              {reviewCardData.propertyAddress}
             </span>
             <div className={styles.rating}>
-              <div className={`${styles.star} ${styles.full}`}></div>
-              <div className={`${styles.star} ${styles.full}`}></div>
-              <div className={`${styles.star} ${styles.full}`}></div>
-              <div className={`${styles.star} ${styles.full}`}></div>
-              <div className={`${styles.star} ${styles.empty}`}></div>
+              {reviewCardData.stars.map((star, i) => (
+                <div
+                  key={star[i]}
+                  className={`${styles.star} ${
+                    star === "1"
+                      ? styles.full
+                      : star === ".5"
+                      ? styles.half
+                      : styles.empty
+                  }`}
+                ></div>
+              ))}
             </div>
           </div>
           <div className={styles.reviewModule}>
             <div>
               <p className={styles.moduleMain}>
-                EUR €{apartment.pricePerNight}
+                EUR €{reviewCardData.pricePerNight}
               </p>
               <p className={styles.moduleSec}>per night</p>
             </div>
             <div>
-              <p className={styles.moduleMain}>{apartment.nightsStay}</p>
+              <p className={styles.moduleMain}>{reviewCardData.nightsStay}</p>
               <p className={styles.moduleSec}>
-                {apartment.nightsStay === "1" ? "night" : "nights"}
+                {reviewCardData.nightsStay === "1" ? "night" : "nights"}
               </p>
             </div>
           </div>
         </div>
         <section
           className={
-            apartment.gallery.length > 2
+            reviewCardData.gallery.length > 2
               ? styles.imageGridThree
               : styles.imageGridTwo
           }
         >
-          {apartment.gallery.map((image) => (
-            <figure key={image} className="photoset-item" data-pswp-uid="13">
+          {reviewCardData.gallery.map((image) => (
+            <figure key={image}>
               <a href="#">
                 <Image
                   src={`${image}`}
-                  alt="Some random image"
+                  alt=""
                   layout="responsive"
                   width={150}
                   height={200}
