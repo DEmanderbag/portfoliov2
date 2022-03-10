49c75f585a69c6c91726e7cebcb37c825a3da595
DEmanderbag <dusan-milosevic@outlook.com>
2022-03-11 00:02:16 +0100

    Update footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,73 +1,105 @@
 import styles from "../../styles/layout/Footer.module.scss";
-
+import Link from "next/link";
 const footerData = [
   {
     question: "Ready to make something kickass?",
     action: "Let's get on a call.",
     name: "Dusan Milosevic",
     email: "dusan-milosevic@outlook.com",
-    links: [
+    social: [
+      {
+        name: "Twitter",
+        path: "/about",
+      },
+      {
+        name: "Github",
+        path: "/about",
+      },
+      {
+        name: "Linkedin",
+        path: "/about",
+      },
+      {
+        name: "Instagram",
+        path: "/about",
+      },
+    ],
+    website: [
+      {
+        name: "Home",
+        path: "/",
+      },
       {
         name: "About",
         path: "/about",
       },
       {
-        name: "Links",
-        path: "/links",
+        name: "Work",
+        path: "/work",
       },
       {
-        name: "Blog",
+        name: "Travelouge",
         path: "/blog",
       },
-      {
-        name: "Projects",
-        path: "/project",
-      },
-      // {
-      //   name: "Contact",
-      //   path: "/contact",
-      // },
-      // {
-      //   name: "Hardware I use",
-      //   path: "/links",
-      // },
-      // {
-      //   name: "Software I use",
-      //   path: "/about",
-      // },
       {
         name: "Bookmarks",
         path: "/bookmarks",
       },
+      // {
+      //   name: "Playground",
+      //   path: "/playground",
+      // },
       {
-        name: "Goals 2022",
-        path: "/goals",
+        name: "Links",
+        path: "/links",
       },
+      // {
+      //   name: "Sitemap",
+      //   path: "/sitemap",
+      // },
     ],
   },
 ];
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       {footerData.map((value) => (
-        <div className={styles.wrapper} key={value}>
+        <div key={value}>
           <div className={styles.message}>
             <h2>{value.question}</h2>
             <h3>{value.action}</h3>
           </div>
-          <div className={styles.group}>
-            <div className={styles.contact}>
-              <p>{value.name}</p>
-              <a href={`mailto:${value.email}`}>{value.email}</a>
-            </div>
+          <div className={styles.contact}>
+            <p>{value.name}</p>
+            <a href={`mailto:${value.email}`}>{value.email}</a>
+          </div>
+          <section className={styles.links}>
+            <div className={styles.separator}></div>
+            <p className={styles.bold}>Website</p>
             <ul className={styles.links}>
-              {value.links.map((link) => (
-                <li key={link.name}>
-                  <a href={link.path}>{link.name}</a>
+              {value.website.map((website) => (
+                <li key={website.name}>
+                  <Link href={website.path}>
+                    <a className={styles.test}>{website.name}</a>
+                  </Link>
                 </li>
               ))}
             </ul>
-          </div>
+          </section>
+          <section className={styles.links}>
+            <div className={styles.separator}></div>
+            <p className={styles.bold}>Social</p>
+            <ul className={styles.links}>
+              {value.social.map((social) => (
+                <li key={social.name}>
+                  <a href={social.path} className={styles.test}>
+                    {social.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </section>
         </div>
       ))}
     </footer>
