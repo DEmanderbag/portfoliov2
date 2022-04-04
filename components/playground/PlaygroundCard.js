89e35c28d011c49b3abadc7c4bf8457b35108e6c
DEmanderbag <dusan-milosevic@outlook.com>
2022-04-04 16:02:19 +0200

    Add missing component

---
+++ components/playground/PlaygroundCard.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "../../styles/playgroud/PlaygroundCard.module.scss";
+
+const PlaygroundCard = ({ data }) => {
+  return (
+    <>
+      {data.map((article) => (
+        <article key={article.name} className={styles.card}>
+          <div className={styles.cardImage}></div>
+          <p className={styles.date}>Published on {article.date}</p>
+          <h2 className={styles.title}>{article.title}</h2>
+          <p className={styles.about}>{article.about}</p>
+          <Link href={`/playground/${article.url}`}>
+            <a className={styles.readMore}>Read more</a>
+          </Link>
+        </article>
+      ))}
+    </>
+  );
+};
+
+export default PlaygroundCard;
